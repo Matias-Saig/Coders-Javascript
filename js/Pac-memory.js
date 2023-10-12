95aaa2c1bd62cf57edd2e27f14db8e7723927ed7
MatiasSaig <saig.matias@gmail.com>
2023-10-12 08:22:00 -0300

    js:funcional + math implementado

--- js/Pac-memory.js
+++ js/Pac-memory.js
@@ -6,7 +6,7 @@ const history = {
 	part2: "Parte II:\n\nEstos fantasmas son Blinky (rojo), Pinky (rosa), Inky (azul) y Clyde (naranja), y cada uno tiene su estrategia única para perseguir a Pac-Man: \n· Blinky, es el más agresivo y siempre trata de acercarse a Pac-Man directamente, su objetivo principal es atrapar a Pac-Man.\n· Pinky, su estrategia puede ser un poco más impredecible, ya que trata de emboscar a Pac-Man posicionándose por delante y cortando su camino.\n· Inky, es un poco más complejo, su movimiento se basa en la posición de Blinky manteniéndose a cierta distancia de este mientras persigue a Pac-Man.\n· Clyde, tiene un comportamiento más errático, a veces persigue a Pac-Man, pero a veces se aleja de él\n\n Estas estrategias combinadas crean un desafío dinámico para el jugador, ya que deben tener en cuenta los movimientos de los fantasmas para evitar ser atrapados mientras recolectan los puntos en el laberinto."
 }
 
-const questions = {
+const questionsOpt1 = {
 
 	nr1: {
 		id: "1",
@@ -22,7 +22,7 @@ const questions = {
 		id: "2",
 		q: "Pregunta 2: Cuál es su objetivo?\n\n A - Comer todos los puntos\n B - Evitar los fantasmas\n C - Ir directo por la superpildora\n D - A y B son correctas",
 		clueA: "es correcta en parte, falta algo más",
-		clueB: this.clueA,
+		clueB: "es correcta en parte, falta algo más",
 		clueC: "no sería lo principal",
 		clueD: null,
 		correct: "D"
@@ -62,7 +62,7 @@ const questions = {
 		id: "6",
 		q: "Pregunta 6: Como es la estrategia de los fantasmas?\n\n A - Pinky busca cortar la huida de Pac-Man posicionándose detrás de él\n B - Clyde es el más predecible y va de frente\n C - Blinky es más errático, a veces se acerca y otras se aleja\n D - Inky se mueve dependiendo de donde este Blinky",
 		clueA: "sería lo contrario",
-		clueB: this.clueA,
+		clueB: "sería lo contrario",
 		clueC: "no es su comportamiento característico",
 		clueD: null,
 		correct: "D"
@@ -77,11 +77,82 @@ const questions = {
 		clueD: "existían los videojuegos en esta década?",
 		correct: "A"
 	}
-
+	
 }
 
+const questionsOpt2 = {
+	
+	nr1: {
+		id: "1",
+		q: "Pregunta 1: Que pasa cuando Pac-Man come la superpildora?\n\n A - Se vuelve invulnerable y puede comer a los fantasmas\n B - Cambia de color y duplica la velocidad\n C - Detiene el tiempo\n D - Aumenta de tamaño y puede comer los fantasmas",
+		clueA: null,
+		clueB: "eso le puede pasar a un erizo azul",
+		clueC: "no sería su función",
+		clueD: "es correcto solo en parte",
+		correct: "A"
+	},
+	
+	nr2: {
+		id: "2",
+		q: "Pregunta 2: Cómo se llaman los fantasmas?\n\n A - Tinky Winky, Dipsy, Laa-Laa, Po\n B - Blinky, Pinky, Inky y Clyde\n C - Leonardo, Donatello, Michelangelo, Raphael\n D - Casper, Slimer, Boo, Canterville",
+		clueA: "Tienen colores pero son Teletubbies, aunque el nombre del primero se parece un poco al de los fantasmas",
+		clueB: null,
+		clueC: "Estas son tortugas, aunque los colores de las máscaras se parecen a los fantasmas",
+		clueD: "Estos fantasmas no son de este juego",
+		correct: "B"
+	},
+	
+	nr3: {
+		id: "3",
+		q: "Pregunta 3: Cómo es Pac-Man?:\n\n A - Naranja y con forma de pizza\n B - Azul y cuadrado\n C - Amarillo y con forma de pizza\n D - Rojo y redondo\n",
+		clueA: "el color no es el correcto",
+		clueB: "es algo más redondeado",
+		clueC: null,
+		clueD: "tiene forma de comida",
+		correct: "C"
+	},
+	
+	nr4: {
+		id: "4",
+		q: "Pregunta 4: Cuál es su objetivo?\n\n A - Comer todos los puntos\n B - Evitar los fantasmas\n C - Ir directo por la superpildora\n D - A y B son correctas",
+		clueA: "es correcta en parte, falta algo más",
+		clueB: "es correcta en parte, falta algo más",
+		clueC: "no sería lo principal",
+		clueD: null,
+		correct: "D"
+	},
 
+	nr5: {
+		id: "5",
+		q: "Pregunta 5: Como es la estrategia de los fantasmas?\n\n A - Pinky busca cortar la huida de Pac-Man posicionándose detrás de él\n B - Clyde es el más predecible y va de frente\n C - Blinky es más errático, a veces se acerca y otras se aleja\n D - Inky se mueve dependiendo de donde este Blinky",
+		clueA: "sería lo contrario",
+		clueB: "sería lo contrario",
+		clueC: "no es su comportamiento característico",
+		clueD: null,
+		correct: "D"
+	},
+	
+	nr6: {
+		id: "6",
+		q: "Pregunta 6: Cual es el color de los fantasmas?\n\n A - Rosa, amarillo, verde, celeste\n B - Rojo, morado, verde, amarillo\n C - Rojo, azul, morado, naranja\n D - Rojo, rosa, azul, naranja",
+		clueA: "Uno de los colores es correcto",
+		clueB: "Dos de los colores que son correctos",
+		clueC: "Se parece, pero este no es",
+		clueD: null,
+		correct: "D"
+	},
+		
+	nr7: {
+		id: "7",
+		q: "Pregunta 7: En que año se lanzo el juego?\n\n A - 1980\n B - 1995\n C - 1960\n D - 1810",
+		clueA: null,
+		clueB: "esta más cerca de la época de la música disco",
+		clueC: "es un par de décadas más adelante",
+		clueD: "existían los videojuegos en esta década?",
+		correct: "A"
+	}
 
+}
 
 /* Funciones */
 
@@ -94,12 +165,24 @@ function selector(answer, opt) {
 	}
 }
 
+function selectorOpt() {
+	let selectorRandom = Math.floor(Math.random()*2);
+	if (selectorRandom < 1) {
+	questions = questionsOpt1;
+	alert("Todo esta en orden")
+	} else {
+		questions = questionsOpt2;
+		alert("Algunas preguntas cambiaron de lugar!")
+	}
+	return questions
+}
+
 // Preguntar y reiterar
 function ask(question, opt1, opt2) {
 	let answer;
 
 	do {
-		answer = prompt(question);
+		answer = prompt(question).trim();
 		if (answer !== opt1 && answer !== opt2) {
 			alert("La respuesta no es correcta. Por favor, elige un número de los asignados");
 		}
@@ -109,7 +192,7 @@ function ask(question, opt1, opt2) {
 }
 
 function gameAsk(question, correct, answer1, clue1, answer2, clue2, answer3, clue3, answer4, clue4) {
-	let answer = prompt(question).toUpperCase();
+	let answer = prompt(question).trim().toUpperCase();
 	const clue0 = "La respuesta no es correcta, puedes intentar una vez más\nPista: "
 	const correctAnswer = "Es la respuesta correcta :)";
 
@@ -131,7 +214,7 @@ function gameAsk(question, correct, answer1, clue1, answer2, clue2, answer3, clu
 				alert("Esa respuesta no es posible... las opciones son A, B, C o D, elige entre ellas");
 				break;
 		}
-		answer = prompt(question).toUpperCase();
+		answer = prompt(question).trim().toUpperCase();
 		if (answer !== correct) {
 			alert("La respuesta es incorrecta. Pasamos a la siguiente pregunta");
 		} else { alert(correctAnswer) }
@@ -194,12 +277,12 @@ function startGame() {
 
 	successAnswer = 0;
 	errorAnswer = 0;
-
+selectorOpt()
 	const introductionQuestion = "\nListo para comenzar? \n\n 1 - Ver tutorial primero \n\n 2 - Empezar a jugar";
 
 	let introductionAnswer = ask(introductionQuestion, answer1, answer2);
 	if (introductionAnswer === "1") {
-		const tutorial = alert("# Tips para el juego:\n\n	- Lee atentamente y presta atención a los detalles\n	- Las preguntas tienen varias opciones, solo tienes que escribir la letra y enviar\n	- Sí te equivocas puedes probar de nuevo una vez\n	- Se contaran los aciertos y errores, pero no los reintentos\n	- Al final se hará una valoración de tu desempeño con emoticones clásicos o retro\n");
+		const tutorial = alert("# Tips para el juego:\n\n	- Lee atentamente y presta atención a los detalles\n	- Las preguntas tienen varias opciones, solo tienes que escribir la letra y enviar\n	- Sí te equivocas puedes probar de nuevo una vez\n	- Se contaran los aciertos y errores, pero no los reintentos\n	- Al final se hará una valoración de tu desempeño con emoticones clásicos o retro\n Las preguntas del juego pueden variar de orden\n");
 	};
 
 	// Historia
@@ -273,6 +356,9 @@ function startGame() {
 
 	sumResultAnswer();
 
+	const prueba = questions.map(e => e.id)
+	alert(prueba)
+
 	// Ending
 	const endingQuestion = "El juego ha terminado ¿como quieres seguir?\n\n 1 - Reiniciar juego\n 2 - Finalizar";
 	const endingAnswer = ask(endingQuestion, answer1, answer2);
@@ -296,6 +382,8 @@ const answerD = "D";
 const answer1 = "1";
 const answer2 = "2";
 
+let questions;
+let gameQuestionId;
 let gameQuestion;
 let gameAnswer;
 
