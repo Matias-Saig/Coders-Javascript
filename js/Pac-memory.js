07147778d1c6663ff07e6f8282e2944d7b43ee91
MatiasSaig <saig.matias@gmail.com>
2023-10-12 05:25:33 -0300

    js:objetos + funciones

--- js/Pac-memory.js
+++ js/Pac-memory.js
@@ -9,23 +9,29 @@ const history = {
 const questions = {
 
 	nr1: {
+		id: "1",
 		q: "Pregunta 1: Cómo es Pac-Man?:\n\n A - Naranja y con forma de pizza\n B - Azul y cuadrado\n C - Amarillo y con forma de pizza\n D - Rojo y redondo\n",
 		clueA: "el color no es el correcto",
 		clueB: "es algo más redondeado",
+		clueC: null,
 		clueD: "tiene forma de comida",
 		correct: "C"
 	},
 
 	nr2: {
+		id: "2",
 		q: "Pregunta 2: Cuál es su objetivo?\n\n A - Comer todos los puntos\n B - Evitar los fantasmas\n C - Ir directo por la superpildora\n D - A y B son correctas",
 		clueA: "es correcta en parte, falta algo más",
 		clueB: this.clueA,
 		clueC: "no sería lo principal",
+		clueD: null,
 		correct: "D"
 	},
 
 	nr3: {
+		id: "3",
 		q: "Pregunta 3: Que pasa cuando Pac-Man come la superpildora?\n\n A - Se vuelve invulnerable y puede comer a los fantasmas\n B - Cambia de color y duplica la velocidad\n C - Detiene el tiempo\n D - Aumenta de tamaño y puede comer los fantasmas",
+		clueA: null,
 		clueB: "eso le puede pasar a un erizo azul",
 		clueC: "no sería su función",
 		clueD: "es correcto solo en parte",
@@ -33,31 +39,39 @@ const questions = {
 	},
 
 	nr4: {
+		id: "4",
 		q: "Pregunta 4: Cómo se llaman los fantasmas?\n\n A - Tinky Winky, Dipsy, Laa-Laa, Po\n B - Blinky, Pinky, Inky y Clyde\n C - Leonardo, Donatello, Michelangelo, Raphael\n D - Casper, Slimer, Boo, Canterville",
 		clueA: "Tienen colores pero son Teletubbies, aunque el nombre del primero se parece un poco al de los fantasmas",
+		clueB: null,
 		clueC: "Estas son tortugas, aunque los colores de las máscaras se parecen a los fantasmas",
 		clueD: "Estos fantasmas no son de este juego",
 		correct: "B"
 	},
 
 	nr5: {
+		id: "5",
 		q: "Pregunta 5: Cual es el color de los fantasmas?\n\n A - Rosa, amarillo, verde, celeste\n B - Rojo, morado, verde, amarillo\n C - Rojo, azul, morado, naranja\n D - Rojo, rosa, azul, naranja",
 		clueA: "Uno de los colores es correcto",
 		clueB: "Dos de los colores que son correctos",
 		clueC: "Se parece, pero este no es",
+		clueD: null,
 		correct: "D"
 	},
 
 	nr6: {
+		id: "6",
 		q: "Pregunta 6: Como es la estrategia de los fantasmas?\n\n A - Pinky busca cortar la huida de Pac-Man posicionándose detrás de él\n B - Clyde es el más predecible y va de frente\n C - Blinky es más errático, a veces se acerca y otras se aleja\n D - Inky se mueve dependiendo de donde este Blinky",
 		clueA: "sería lo contrario",
 		clueB: this.clueA,
 		clueC: "no es su comportamiento característico",
+		clueD: null,
 		correct: "D"
 	},
 
 	nr7: {
+		id: "7",
 		q: "Pregunta 7: En que año se lanzo el juego?\n\n A - 1980\n B - 1995\n C - 1960\n D - 1810",
+		clueA: null,
 		clueB: "esta más cerca de la época de la música disco",
 		clueC: "es un par de décadas más adelante",
 		clueD: "existían los videojuegos en esta década?",
@@ -68,8 +82,8 @@ const questions = {
 
 
 
-/* Funciones */
 
+/* Funciones */
 
 // Selector
 function selector(answer, opt) {
@@ -96,33 +110,35 @@ function ask(question, opt1, opt2) {
 
 function gameAsk(question, correct, answer1, clue1, answer2, clue2, answer3, clue3, answer4, clue4) {
 	let answer = prompt(question).toUpperCase();
+	const clue0 = "La respuesta no es correcta, puedes intentar una vez más\nPista: "
+	const correctAnswer = "Es la respuesta correcta :)";
 
 	if (answer !== correct) {
 		switch (answer) {
 			case answer1:
-				alert("La respuesta no es correcta, puedes intentar una vez más\nPista: " + clue1);
+				alert(clue0 + clue1);
 				break;
 			case answer2:
-				alert("La respuesta no es correcta, puedes intentar una vez más\nPista: " + clue2);
+				alert(clue0 + clue2);
 				break;
 			case answer3:
-				alert("La respuesta no es correcta, puedes intentar una vez más\nPista: " + clue3);
+				alert(clue0 + clue3);
 				break;
 			case answer4:
-				alert("La respuesta no es correcta, puedes intentar una vez más\nPista: " + clue4);
+				alert(clue0 + clue4);
 				break;
 			default:
-				alert("Esa no era una respuesta posible");
+				alert("Esa respuesta no es posible... las opciones son A, B, C o D, elige entre ellas");
 				break;
 		}
 		answer = prompt(question).toUpperCase();
 		if (answer !== correct) {
 			alert("La respuesta es incorrecta. Pasamos a la siguiente pregunta");
-		} else { alert("Es la respuesta correcta :)") }
+		} else { alert(correctAnswer) }
 		return answer
 
 	} else {
-		alert("Es la respuesta correcta :)");
+		alert(correctAnswer);
 		return answer;
 	}
 
@@ -160,9 +176,6 @@ function sumResultAnswer() {
 		case 7:
 			alert(result + "... :D ... Perfecto, 7 de 7!!!");
 			return;
-		default:
-			alert("ups! algo salio mal, parece que hubo 0 aciertos");
-			return;
 	}
 
 }
@@ -188,34 +201,68 @@ function startGame() {
 	alert(history.part1);
 	alert(history.part2);
 
-
-
-
-	1
-
-	resultAnswer();
-
-	2
-
-	resultAnswer();
-
-	3
+	gameQuestionId = questions.nr1.id;
+	gameQuestion = questions.nr1.q;
+	clueA = questions.nr1.clueA
+	clueB = questions.nr1.clueB
+	clueC = questions.nr1.clueC
+	clueD = questions.nr1.clueD
+	correct = questions.nr1.correct;
 
 	resultAnswer();
 
-	4
+	gameQuestionId = questions.nr2.id;
+	gameQuestion = questions.nr2.q;
+	clueA = questions.nr2.clueA
+	clueB = questions.nr2.clueB
+	clueC = questions.nr2.clueC
+	clueD = questions.nr2.clueD
+	correct = questions.nr2.correct;
 
 	resultAnswer();
-
-	5
+gameQuestionId = questions.nr3.id;
+	gameQuestion = questions.nr3.q;
+	clueA = questions.nr3.clueA
+	clueB = questions.nr3.clueB
+	clueC = questions.nr3.clueC
+	clueD = questions.nr3.clueD
+	correct = questions.nr3.correct;
 
 	resultAnswer();
-
-	6
+gameQuestionId = questions.nr4.id;
+	gameQuestion = questions.nr4.q;
+	clueA = questions.nr4.clueA
+	clueB = questions.nr4.clueB
+	clueC = questions.nr4.clueC
+	clueD = questions.nr4.clueD
+	correct = questions.nr4.correct;
 
 	resultAnswer();
+gameQuestionId = questions.nr5.id;
+	gameQuestion = questions.nr5.q;
+	clueA = questions.nr5.clueA
+	clueB = questions.nr5.clueB
+	clueC = questions.nr5.clueC
+	clueD = questions.nr5.clueD
+	correct = questions.nr5.correct;
 
-	7
+	resultAnswer();
+gameQuestionId = questions.nr6.id;
+	gameQuestion = questions.nr6.q;
+	clueA = questions.nr6.clueA
+	clueB = questions.nr6.clueB
+	clueC = questions.nr6.clueC
+	clueD = questions.nr6.clueD
+	correct = questions.nr6.correct;
+
+	resultAnswer();
+gameQuestionId = questions.nr7.id;
+	gameQuestion = questions.nr7.q;
+	clueA = questions.nr7.clueA
+	clueB = questions.nr7.clueB
+	clueC = questions.nr7.clueC
+	clueD = questions.nr7.clueD
+	correct = questions.nr7.correct;
 
 	resultAnswer();
 
@@ -231,6 +278,8 @@ function startGame() {
 
 let successAnswer = 0;
 let errorAnswer = 0;
+
+let userAnswers = [];
 
 const answerA = "A";
 const answerB = "B";
